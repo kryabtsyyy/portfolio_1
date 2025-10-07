14045f5580cc8d0b1a85d79cb5cdcc8acd8bd44c
MSI <kryabtsy@gmail.com>
2025-10-07 18:31:38 +0300

    add data to script, add skilllist, change paths

---
+++ js/script.js
@@ -0,0 +1,64 @@
+const data = [
+    {
+        // Название навыка 
+        name: 'HTML',
+        //Хранение файла иконки
+        iconName: 'skill=html.svg', 
+        // Уровень владения (число от 0 до 100)
+        level: 30, 
+        // Уникальный CSS-класс 
+        className: 'skill-html'
+    },
+    {
+        name: 'CSS',
+        iconName: 'skill=css.svg',
+        level: 20,
+        className: 'skill-css'
+    },
+    {
+        name: 'Python',
+        iconName: 'skill=python.svg',
+        level: 50,
+        className: 'skill-python'
+    },
+    {
+        name: 'C++',
+        iconName: 'skill=c++.svg',
+        level: 70,
+        className: 'skill-cpp'
+    }
+];
+
+const skillList = document.querySelector('.skill-list');
+// Создание элементов
+data.forEach(skill => {
+    const skillNameElement = document.createElement('dt'); // Название навыка (HTML, CSS и т.д.)
+    const skillBarContainer = document.createElement('dd');  // Контейнер для полосы прогресса
+    const skillBar = document.createElement('div');          // Полоса прогресса
+
+    // Настройка элементов 
+    
+    // Название навыка (dt)
+    skillNameElement.textContent = skill.name;
+
+    //Интерполяция для создания пути
+    skillNameElement.style.backgroundImage = `url("../img/${skill.iconName}")`;
+
+    // Контейнер полосы прогресса (dd)
+    skillBarContainer.classList.add('skill-bar-container'); 
+    
+    // Добавляем уникальный класс для конкретного навыка 
+    skillBar.classList.add('skill-bar', skill.className);
+    
+    // Установка ширины полосы (в процентах)
+    skillBar.style.width = `${skill.level}%`;
+    
+    // Добавляем текст
+    skillBar.textContent = `${skill.level}%`; 
+
+    // Вложение элементов
+    skillBarContainer.append(skillBar); 
+
+    // Добавление элементов на страницу
+    skillList.append(skillNameElement, skillBarContainer);
+});
