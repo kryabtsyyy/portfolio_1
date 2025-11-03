d2ffddce415feb1e99d4ed815fc16e32ec93079e
MSI <kryabtsy@gmail.com>
2025-11-03 13:41:50 +0300

    making numbers invisible

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ const skills = {
 
             const levelBlock = document.createElement('div');
             levelBlock.style.width = `${skillData.value}%`;
-            levelBlock.innerHTML = `${skillData.value}%`;
+            //levelBlock.innerHTML = `${skillData.value}%`;
 
             levelElement.append(levelBlock);
 
