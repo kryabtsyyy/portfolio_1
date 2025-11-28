fc8f9e0c88a673018b63a09d379b7fc6130652a3
MSI <kryabtsy@gmail.com>
2025-11-28 15:50:10 +0300

    2.4 test 1

--- js/script.js
+++ js/script.js
@@ -6,7 +6,10 @@ const skills = {
         { skillName: 'cpp', value: 70, icon: 'python.svg' }
     ],
 
+    sortType : null,
+
     generateList(skillList) {
+        skillList.innerHTML = '';
         this.data.forEach(skillData => {
             const nameElement = document.createElement('dt');
             nameElement.classList.add(`skill-item`)
@@ -21,12 +24,58 @@ const skills = {
             //levelBlock.innerHTML = `${skillData.value}%`;
 
             levelElement.append(levelBlock);
-
             skillList.append(nameElement, levelElement);
         })
-    }
+    },
+
+    sortList(sortType){
+        if(this.sortType === sortType){
+            this.data.reverse();
+        }
+        else{
+            switch(sortType){
+                case 'name':
+                    skills.data.sort(getComparer('skillName'));
+                    break;
+                
+                case 'level':
+                    skills.data.sort(getComparer('value'));
+                    break;
+
+                default:
+                    return;
+            }
+
+            this.sortType = sortType;
+        }
+
+        this.generateList(skillList);
+        }
 }
 
 const skillList = document.querySelector('.skill-list')
 
 skills.generateList(skillList)
+
+const sortBlock = document.querySelector('.skill-sort')
+sortBlock.addEventListener('click', (e) => {
+    if (e.target.nodeName !== "BUTTON"){
+        return;
+    }
+
+    const sortType = e.target.dataset.type;
+
+    skills.sortList(sortType);
+});
+
+function getComparer(prop){
+    return function(a, b){
+        if(a[prop] < b[prop]){
+            return -1;
+        }
+        if(a[prop] > b[prop]){
+            return 1;
+        }
+        return 0;
+    }
+}
